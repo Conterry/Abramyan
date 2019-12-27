9df270175fda1225b99f5f7b84206632a0a5e3a0
Conterry <glebois05@gmail.com>
2019-12-27 18:59:39 +0200

    For and Case

---
+++ Case_Top/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Case_Top
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        static void Case10()
+        {
+            int a = -1;
+            switch (a)
+            {
+                case 1:
+                    Console.WriteLine("В");
+                    break;
+                case 0:
+                    Console.WriteLine("C");
+                    break;
+                case -1:
+                    Console.WriteLine("З");
+                    break;
+            }
+        }
+
+        static void Case11()
+        {
+
+        }
+    }
+}
---
+++ For_Top/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace For_Top
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            For2();
+        }
+
+        static void For1()
+        {
+            int n=3, k=4;
+            for(int i=0; i<n; i++)
+            {
+                Console.WriteLine(k);
+            }
+        }
+
+        static void For2()
+        {
+            int a=3, b=7, n=0;
+            for(int i=a; i<=b; i++, n++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine(n);
+        }
+    }
+}
