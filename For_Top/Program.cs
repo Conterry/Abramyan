1104735717aa0622642ed7654aa0cc072b4aedc4
Conterry <glebois05@gmail.com>
2019-12-29 21:16:10 +0200

    done For10 - For15

--- For_Top/Program.cs
+++ For_Top/Program.cs
@@ -105,5 +105,66 @@ namespace For_Top
             }
             Console.WriteLine(s);
         }
+
+        static void For11()
+        {
+            int n = 5;
+            double s=0;
+            for(int i=0; i<=n; i++)
+            {
+                s += (n + i) * (n + i);
+            }
+            Console.WriteLine(s);
+        }
+
+        static void For12()
+        {
+            int n = 5;
+            double d = 1;
+            for(float i=1.1f, j=1; j<n; i+=0.1f, j++)
+            {
+                d *= i;
+            }
+            Console.WriteLine(d);
+        }
+
+        static void For13()
+        {
+            int n = 5;
+            double d = 1;
+            for (float i = 1.1f, j = 1; j < n; i += 0.1f, j++)
+            {
+                if (j % 2 == 0)
+                {
+                    d += i;
+                }
+                else
+                {
+                    d -= i;
+                }
+            }
+            Console.WriteLine(d);
+        }
+
+        static void For14()
+        {
+            int n = 5, s = 0;
+            for(int i=1; i<2*n; i += 2)
+            {
+                s += i;
+            }
+            Console.WriteLine(s);
+        }
+
+        static void For15()
+        {
+            int n = 5;
+            double a = 3.5, f=1;
+            for(int i=0; i<n; i++)
+            {
+                f *= a;
+            }
+            Console.WriteLine(f);
+        }
     }
 }
