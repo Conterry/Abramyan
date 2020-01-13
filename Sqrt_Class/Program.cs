72626d0f931cbcb224a60415cbbd56c4b15ece41
Conterry <glebois05@gmail.com>
2020-01-13 22:53:36 +0200

    ...

---
+++ Sqrt_Class/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SqrtSearch_Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bin();
+        }
+
+        static void Bin()
+        {
+            double k = 9, x = k / 2;
+            while (x*x != k)
+            {
+                if (x * x < k)
+                {
+                    x = x * 3 / 2;
+                }
+                else
+                {
+                    x /= 2;
+                }
+            }
+            Console.WriteLine(x);
+        }
+    }
+}
