cf1cb1df8d8c737aa8f8a5b1d3c4c6f090e004e5
Conterry <glebois05@gmail.com>
2020-01-15 20:40:58 +0200

    ...

--- Case_Top/Program.cs
+++ Case_Top/Program.cs
@@ -26,7 +26,73 @@ namespace Case_Top
 
         static void Case11()
         {
+            char c = 'N';
+            int n1 = 2, n2 = -1;
+            WayOut(c, n1);
+            WayOut(c, n2);
+        }
 
+        static void WayOut(char c, int n)
+        {
+            switch(c)
+            {
+                case 'N':
+                    switch (n)
+                    {
+                        case 1:
+                            c = 'W';
+                            break;
+                        case -1:
+                            c = 'E';
+                            break;
+                        case 2:
+                            c = 'S';
+                            break;
+                    }
+                    break;
+                case 'E':
+                    switch (n)
+                    {
+                        case 1:
+                            c = 'N';
+                            break;
+                        case -1:
+                            c = 'S';
+                            break;
+                        case 2:
+                            c = 'W';
+                            break;
+                    }
+                    break;
+                case 'S':
+                    switch (n)
+                    {
+                        case 1:
+                            c = 'E';
+                            break;
+                        case -1:
+                            c = 'W';
+                            break;
+                        case 2:
+                            c = 'N';
+                            break;
+                    }
+                    break;
+                case 'W':
+                    switch (n)
+                    {
+                        case 1:
+                            c = 'S';
+                            break;
+                        case -1:
+                            c = 'N';
+                            break;
+                        case 2:
+                            c = 'E';
+                            break;
+                    }
+                    break;
+            }
         }
     }
 }
