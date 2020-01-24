da150d5a724e3ed3555a35348a1b68482afac55b
Conterry <glebois05@gmail.com>
2020-01-24 21:57:06 +0200

    WPI

---
+++ Series_Top/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace Series_Top
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Series2();
+        }
+
+        static void Series1()
+        {
+            int[] a = { 2, 4, 7, 3, 6, 8, 6, 3, 6, 8 };
+            int s = 0;
+            for(int i = 0; i < a.Length; i++)
+            {
+                s += a[i];
+            }
+            Console.WriteLine(s);
+        }
+
+        static void Series2()
+        {
+            int n = 10;
+            int[] a = new int[n];
+            int s = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                a[i] = Console.Read();
+                s += a[i];
+            }
+            Console.WriteLine(s);
+        }
+
+        static void Series3()
+        {
+            int[] a = { 2, 4, 7, 3, 6, 8, 6, 3, 6, 8 };
+            int s = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                s += a[i];
+            }
+            Console.WriteLine(s/10);
+        }
+
+        static void Series4()
+        {
+            int n=10;
+            int[] a = new int[] {3,7,3,6,4,6,4,8,5,4 };
+            int s = 0, d=1;
+            for (int i = 0; i < a.Length; i++)
+            {
+                s += a[i];
+                d *= a[i];
+            }
+            Console.WriteLine(s);
+            Console.WriteLine(d);
+        }
+
+        static void Series5()
+        {
+            double[] a = { 3.4, 4.5, 5.6, 6.7 };
+            int s = 0;
+            for(int i = 0; i < a.Length; i++)
+            {
+                int f = (int)a[i];
+                s += f;
+                Console.WriteLine(f);
+            }
+            Console.WriteLine(s);
+        }
+
+        static void Series6()
+        {
+            double[] a = { 3.4, 4.5, 5.6, 6.7 };
+            double s = 1;
+            for (int i = 0; i < a.Length; i++)
+            {
+                double f = a[i] *10 %10;
+                s *= f;
+                Console.WriteLine(f);
+            }
+            Console.WriteLine(s);
+        }
+
+        static void Series7()
+        {
+        }
+    }
+}
